0b091f805aab1e4c0e7e40b334f30a4b9d15ff7c
Beatriz Chamsim <biachamsim@gmail.com>
2022-04-04 11:29:56 -0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,76 @@
+let contador = 0
+
+let pratoEscolhido = ''
+let precoPratoEscolhido = ''
+let bebidaEscolhida = ''
+let precoBebidaEscolhida = ''
+let sobremesaEscolhida = ''
+let precoSobremesaEscolhida = ''
+
+
+
+function selecionarComida(prato, descricao, preco){
+
+  desmarcarProduto('.prato')
+  const selecionado = document.querySelector("." + prato);
+  selecionado.classList.add("selecao");
+
+  contador = contador + 1;
+  
+  pratoEscolhido = descricao;
+  precoPratoEscolhido = preco;
+
+  finalizarPedido();
+  
+}
+
+function selecionarBebida(bebida, descricao, preco){
+
+    desmarcarProduto('.bebida')
+    const selecionado = document.querySelector("." + bebida);
+    selecionado.classList.add("selecao");
+
+    contador = contador + 1;
+    
+    bebidaEscolhida = descricao;
+    precoBebidaEscolhida = preco; 
+
+    finalizarPedido();
+}
+
+function selecionarSobremesa(sobremesa, descricao, preco){
+
+    desmarcarProduto('.sobremesa')
+    const selecionado = document.querySelector("." + sobremesa);
+    selecionado.classList.add("selecao");
+
+    contador = contador + 1;
+    
+    sobremesaEscolhida = descricao;
+    precoSobremesaEscolhida = preco;
+
+    finalizarPedido();
+}
+
+
+function desmarcarProduto(produto){
+    const selecionado = document.querySelector(produto).querySelector(".selecao");
+    console.log("ola")
+    if (selecionado !== null){
+        selecionado.classList.remove("selecao")  
+        contador = contador -1; 
+    } 
+
+}
+
+function finalizarPedido(){
+    const botaoFinal = document.querySelector('button')
+    if(contador===3){
+        
+        botaoFinal.classList.add("final")
+
+        botaoFinal.innerHTML='Fechar pedido'
+    }
+}
+
+
